c2d89333bff77bc518125a0a2159383128db65f9
huyougu <1484786127@qq.com>
2024-03-01 12:27:01 +0800

    样式

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,6 @@ import Header from './components/Header.vue'
 <style scoped>
 
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
